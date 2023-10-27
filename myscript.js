9a17558f9f0687a8ad293638abf20628926b82f6
Abdulliadi52 <abdulliadi3@gmail.com>
2023-10-27 09:20:45 +0100

    changes

--- myscript.js
+++ myscript.js
@@ -2,6 +2,15 @@ const buttons = document.getElementById("btn");
 const navBar = document.getElementById("navbar");
 
 buttons.addEventListener("click", () => {
-  buttons.classList.toggle("active");
   navBar.classList.toggle("active");
+
+  if (buttons.src.endsWith("images/icon-hamburger.svg")) {
+    buttons.src = "images/close-line.svg"; // Change to the new image source
+    buttons.style.width = "30px";
+    buttons.style.height = "30px";
+  } else {
+    buttons.src = "images/icon-hamburger.svg"; // Revert to the original image source
+    buttons.style.width = "20px";
+    buttons.style.height = "18px";
+  }
 });
